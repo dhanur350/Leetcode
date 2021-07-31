5d3ab9a48cb2074558ded842f0a6bacf631b9b8a
Dhanur <dhanur297@gmail.com>
2021-07-31 23:29:20 +0530

    HW_day_2

---
+++ 3sum.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        int n, i, j, k;
+        n = nums.size();
+        if (n < 3)
+            return {};
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for (i = 0; i < n - 2; ++i)
+        {
+            if (i == 0 || nums[i] != nums[i - 1])
+                j = i + 1;
+            k = n - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0)
+                {
+                    result.push_back({nums[i], nums[j], nums[k]});
+                    while (j < k && nums[j] == nums[j + 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k - 1])
+                        k--;
+                    j++;
+                    k--;
+                }
+                else if (sum > 0)
+                    k--;
+                else
+                    j++;
+            }
+        }
+        return result;
+    }
+};
